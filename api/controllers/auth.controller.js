50780340a95ced10df448ff4323856cd2be2b477
AnshikaJaiswal-Hub <anshika5873art2001@gmail.com>
2025-03-21 15:34:33 +0530

    Auth controller - Login

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,74 @@
-export const register = (req, res) => {
-  //db operations
-  console.log(req.body);
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import prisma from "../lib/prisma.js";
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    //HASH THE PASSWORD
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+
+    //CREATE A NEW USER AND SAVE TO DB
+    const newUSer = await prisma.user.create({
+      data: { username, email, password: hashedPassword },
+    });
+
+    console.log(newUSer);
+    res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to create user" });
+  }
 };
 
-export const login = (req, res) => {};
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    //check If the user exists
+
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) return res.status(401).json({ message: "Invalid Credentials!" });
+
+    //check if the password is correct
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid)
+      return res.status(401).json({ message: "Invalid Credentials!" });
+
+    //generate cookie token and send to the user
+
+    //res.setHeader("Set-Cookie", "test=" + "myValue").json("Success");
+
+    const age = 1000 * 60 * 60 * 24 * 7;
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: age,
+      }
+    );
+
+    res
+      .cookie("Token", token, {
+        httpOnly: true,
+        //secure:true
+        maxAge: age,
+      })
+      .status(200)
+      .json("Login Successful");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to Login!" });
+  }
+};
 
 export const logout = (req, res) => {};
